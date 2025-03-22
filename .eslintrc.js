1e47bbf235e96db279f7f57c7e1325094a128475
Vijay Dhama <vjdhama26@gmail.com>
2025-03-22 22:07:45 +0530

    Add ESLint configuration to fix CI linting errors

---
+++ .eslintrc.js
@@ -0,0 +1,21 @@
+module.exports = {
+  env: {
+    node: true,
+    es2021: true,
+    jest: true
+  },
+  extends: [
+    'eslint:recommended'
+  ],
+  parserOptions: {
+    ecmaVersion: 2021,
+    sourceType: 'module'
+  },
+  rules: {
+    'no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+    'no-console': 'off',
+    'semi': ['error', 'always'],
+    'quotes': ['error', 'single', { avoidEscape: true }]
+  },
+  ignorePatterns: ['dist/**/*']
+};
